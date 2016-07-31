f1454dd5c57ffd8f2ba41127dbe004547e86b5d4
Mohamed Habashy <mohamed.habshey10@gmail.com>
2016-07-31 23:32:24 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+var Promise = require('bluebird');
+
+
+
+seneca = require('seneca')({
+  timeout: 30000,
+  tag: 'base node',
+}).use('mesh',
+  {
+     port: process.env.PORT||39999,
+        isbase: true,
+  });
+
+seneca.pact = Promise.promisify(seneca.act, { context: seneca });
+
+// set interval for every 9 sec send the watchdog signal
+setInterval(function(){
+seneca.pact({ role: 'system', cmd: 'watchdog' })
+    .then(function (live) {
+        console.log("Live Nodes: "+ JSON.stringify( live));
+    });
+}, 9000);
+
+module.exports = seneca;
