3566009c653bf1b25ae73003c94700170ff494b4
Mohamed Habashy <mohamed.habshey10@gmail.com>
2016-07-31 23:49:54 +0200

    created simple server to bind port to

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 var Promise = require('bluebird');
 
-
+//Lets require/import the HTTP module
+var http = require('http');
 
 seneca = require('seneca')({
   timeout: 30000,
@@ -22,5 +23,20 @@ seneca.pact({ role: 'system', cmd: 'watchdog' })
     });
 }, 9000);
 
-console.log("port: "+process.env.PORT||39999)
+
+var PORT = process.env.PORT||39999;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: ", PORT);
+});
 module.exports = seneca;
